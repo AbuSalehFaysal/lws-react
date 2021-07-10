bbb04dd1a1fcb7444ae034a14570cccd8071d18c
Abu Saleh  Faysal <asfaysal.bracu@gmail.com>
2021-07-11 00:31:39 +0600

    lifting state up

---
+++ src/components/TemperatureInput.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const scaleNames = {
+    c: 'Celsius',
+    f: '',
+};
+
+export default function TemperatureInput({ temperature, scale, onTemperatureChange }) {
+    return (
+        <fieldset>
+            <legend>Enter Temperature in {scaleNames[scale]}: </legend>
+            <input
+                type="text"
+                value={temperature}
+                onChange={(e) => onTemperatureChange(e, scale)}
+            />
+        </fieldset>
+    );
+}
