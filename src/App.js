b9f53ee783fb876eba10986647df8a5e172095af
Abu Saleh  Faysal <asfaysal.bracu@gmail.com>
2021-07-25 15:21:45 +0600

    context api

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
+import React from 'react';
 import ClickCounter from './components/ClickCounter';
 import Counter from './components/Counter';
-import HoverCounter from './components/HoverCounter';
+import Section from './components/Section';
+import ThemeContext from './contexts/themeContext';
 
-function App() {
-    return (
-        <div className="App">
-            <Counter>
-                {(counter, incrementCount) => (
-                    <ClickCounter count={counter} incrementCount={incrementCount} />
-                )}
-            </Counter>
-            <Counter>
-                {(counter, incrementCount) => (
-                    <HoverCounter count={counter} incrementCount={incrementCount} />
-                )}
-            </Counter>
-        </div>
-    );
+export default class App extends React.Component {
+    state = {
+        theme: 'dark',
+    };
+
+    render() {
+        const { theme } = this.state;
+        return (
+            <div className="App">
+                <Counter>
+                    {(counter, incrementCount) => (
+                        <ClickCounter count={counter} incrementCount={incrementCount} />
+                    )}
+                </Counter>
+                <ThemeContext.Provider value={{ theme }}>
+                    <Section />
+                </ThemeContext.Provider>
+            </div>
+        );
+    }
 }
-
-export default App;
--- src/components/HoverCounter.js
+++ src/components/HoverCounter.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
-export default function HoverCounter({ count, incrementCount }) {
+export default function HoverCounter({ count, incrementCount, theme }) {
+    const style = theme === 'dark' ? { backgroundColor: '#000000', color: '#ffffff' } : null;
     return (
         <div>
-            <h1 onMouseOver={incrementCount}>Hover {count} Times</h1>
+            <h1 onMouseOver={incrementCount} style={style}>
+                Hover {count} Times
+            </h1>
         </div>
     );
 }
